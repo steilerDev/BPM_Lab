b87a83697f78c142e6efe91e260cb249e55cd54c
Frank Steiler <frank@steilerdev.de>
2016-12-14 22:48:29 +0100

    Code cleanup & web app nearly working, success check & summary missing.

--- T1-SOGShop-WebApp/js/main.js
+++ T1-SOGShop-WebApp/js/main.js
@@ -13,7 +13,6 @@ function updateProducts() {
         },
 
         success: function (soapResponse) {
-            console.log(soapResponse.toJSON());
             $('#spinner').spin(false);
             $('#items').html("");
 
@@ -21,7 +20,12 @@ function updateProducts() {
             for(var i in productList) {
                 $('#items').append(
                     '<div class="col-sm-6 col-md-4">' +
-                    '<div class="thumbnail equalize">' +
+                    '<div class="thumbnail equalize product" ' +
+                        'data-id="' + productList[i]["FlowSOG:id"] + '"' +
+                        'data-name="' + productList[i]["FlowSOG:name"] + '"' +
+                        'data-price="' + productList[i]["FlowSOG:price"] + '"' +
+                        'data-weight="' + productList[i]["FlowSOG:weight"] + '"' +
+                    '">' +
                     '<div class="caption">' +
                     '<h4>' + productList[i]["FlowSOG:name"] + '</h4>' +
                     '<p>Price: ' + productList[i]["FlowSOG:price"] + '€<p>' +
@@ -29,8 +33,8 @@ function updateProducts() {
                     '<div class="input-group">' +
                     '<input type="text" class="form-control" placeholder="Quantity">' +
                     '<span class="input-group-btn">' +
-                    '<button class="btn btn-default" type="button">+</button>' +
-                    '<button class="btn btn-default" type="button">-</button>' +
+                    '<button class="btn btn-default btn-plus" type="button">+</button>' +
+                    '<button class="btn btn-default btn-minus" type="button">-</button>' +
                     '</span>' +
                     '</div>' +
                     '</div>' +
@@ -38,21 +42,168 @@ function updateProducts() {
                 )
             }
             var maxHeight = 0;
-            $(".equalize").each(function(){
+            $('.equalize').each(function(){
                 if ($(this).height() > maxHeight) { maxHeight = $(this).height(); }
             });
-            $(".equalize").height(maxHeight);
+            $('.equalize').height(maxHeight);
 
-            // do stuff with soapResponse
-            // if you want to have the response as JSON use soapResponse.toJSON();
-            // or soapResponse.toString() to get XML string
-            // or soapResponse.toXML() to get XML DOM
+            $('.btn-plus').click(function () {
+                var inputField = $(this).parent().parent().find("input");
+                var inputValue = parseInt(inputField.val()) || 0;
+                inputField.val(++inputValue);
+            });
+
+
+            $('.btn-minus').click(function () {
+                var inputField = $(this).parent().parent().find("input");
+                var inputValue = parseInt(inputField.val()) || 0;
+                inputField.val(inputValue > 0? --inputValue: 0);
+            });
         },
         error: function (soapResponse) {
-            console.log(soapResponse.toJSON());
+            $('#spinner').spin(false);
+            console.log(soapResponse);
             // show error
         }
     });
+}
+
+function onCheckoutClick() {
+    $('#checkoutBtn').click(function () {
+        $('#checkout').modal('show');
+        $('#shopping-card').html("");
+        $('#shopping-card').append(
+            '<tr>' +
+            '<th>Name</th>' +
+            '<th>Price</th>' +
+            '<th>Weight</th>' +
+            '<th>Quantity</th>' +
+            '<th></th>' +
+            '</tr>'
+        );
+
+        $('.product').each(function () {
+            var qty = parseInt($(this).find("input").val()) || 0;
+            if (qty > 0) {
+                var id = $(this).data("id"),
+                    name = $(this).data("name"),
+                    price = $(this).data("price"),
+                    weight = $(this).data("weight");
+
+                $('#shopping-card').append(
+                    '<tr '+
+                    'data-id="' + id + '"' +
+                    'data-name="' + name + '"' +
+                    'data-price="' + price + '"' +
+                    'data-weight="' + weight + '"' +
+                    '>' +
+                    '<td>' + name + '</td>' +
+                    '<td>' + price + ' €</td>' +
+                    '<td>' + weight + ' gr</td>' +
+                    '<td><input type="text" class="form-control" value="' + qty + '" placeholder="Quantity"></td>' +
+                    '<td><button type="button" class="btn btn-danger delete-item"><span class="glyphicon glyphicon-trash"></span></button></td>' +
+                    '</tr>'
+                );
+            }
+        });
+
+        $('.delete-item').click(function() {
+            $(this).parent().parent().remove();
+        })
+
+    });
+}
+
+function onBuyClick() {
+    $('#buyBtn').click(function () {
+        var products = [];
+        $('#shopping-card').children().children().each(function () {
+            var qty = parseInt($(this).find("input").val()) || 0;
+            if (qty > 0) {
+                products.push({
+                    product: {
+                        productId: $(this).data("id"),
+                        numberOfItems: qty
+                    }
+                });
+            }
+        });
+        //Todo: Add checks for empty bag & input fields
+        var port = $('#port').val()
+        var method = $('input[name=method]:checked').val();
+
+
+        var data = {
+            input: {
+                customerId: $('#email').val(),
+                shippingAddress: $('#address').val(),
+                paymentDetails: {
+                    bankName: $('#bankName').val(),
+                    bankAddress: $('#bankAddress').val(),
+                    accountNumber: $('#accountNumber').val(),
+                    accountHolderName: $('#accountHolder').val()
+                },
+                shipmentDate: getCurrentDate(),
+                products: products
+            }
+        };
+
+        if(method == "/ode/processes/ConductOrder-Async") {
+            data.callbackPort = port;
+        }
+
+        toggleCheckoutScreen();
+
+        $('#order-processing-content').spin(spinnerOpts);
+        $.soap({
+            url: 'http://uni.steilergroup.net:' + port + method,
+            method: 'ConductOrderRequest',
+            appendMethodToURL: false,
+            namespaceURL: 'http://group-one',
+
+            data: data,
+            success: function(soapResponse) {
+                $('#order-processing-content').spin(false);
+                $('#doneBtn').removeAttribute("disabled");
+                var soapJsonResponse soapResponse.toJSON();
+
+                if(soapJsonResponse["#document"]["soapenv:Envelope"]["soapenv:Body"]["ConductOrderResponse"]["tns:order"]["tns:status"] == "cancelled") {
+
+                }
+
+                console.log(soapResponse.toJSON())
+            },
+            error: function(soapResponse) {
+                $('#spinner').spin(false);
+                console.log(soapResponse)
+            }
+        });
+    });
+}
+
+function toggleCheckoutScreen() {
+    $('#order-preparation-content').toggleClass("hidden");
+    $('#order-processing-content').toggleClass("hidden");
+    $('#buyBtn').toggleClass("hidden");
+    $('#doneBtn').toggleClass('hidden');
+    $('#cancelCheckoutButton').toggleClass("hidden");
+}
+
+function getCurrentDate() {
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth()+1; //January is 0!
+    var yyyy = today.getFullYear();
+
+    if(dd<10) {
+        dd='0'+dd
+    }
+
+    if(mm<10) {
+        mm='0'+mm
+    }
+
+    return yyyy+'-'+mm+'-'+dd;
 }
 
 $(document).ready(function() {
@@ -62,4 +213,12 @@ $(document).ready(function() {
         limit = parseInt($(this).text()) || limit;
         updateProducts()
     });
+
+    $('#doneBtn').click(function () {
+       toggleCheckoutScreen();
+        $('#doneBtn').attr("disabled", "disabled");
+    });
+
+    onCheckoutClick();
+    onBuyClick();
 });
